ee0f2c69d0ecf53c1c04031ea84ee5f811523ea8
akbarismail <ismailakbar.me@gmail.com>
2022-12-14 16:27:21 +0700

    update url netlify in App.js

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = 'YOURDEPLOYURL'; // TODO: replace this
+const url = 'https://gallery-app-production-akbarismail.netlify.app/'; // TODO: replace this
 const studentName = 'Akbar Ismail'; // TODO: replace this
 const studentId = 'FE2541268'; // TODO: replace this
 
